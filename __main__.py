a6ed9d08cf08313e5ad1a5234d9ed4837b24405e
FickleFeline <N/A>
2024-06-20 12:17:53 +0200

    snapshot before implementing csv handling

--- __main__.py
+++ __main__.py
@@ -1,8 +1,17 @@
-import configparser
-import time
-from datetime import timedelta
+from pathlib import Path # to get location of home directory
+import configparser # to create / read config file
+import csv # to read / write log dictionaries
+import time # for time stuff
+from datetime import timedelta # also for time stuff
 # import textual
 # import argparse
+
+class TimeEntry:
+   def __init__(self, start: str, end: str, desc: str, tags: str):
+      self.start = start
+      self.end = end
+      self.desc = desc
+      self.tags = tags
 
 def calcTimeDiff(start: time.struct_time, end: time.struct_time):
    '''
@@ -13,25 +22,44 @@ def calcTimeDiff(start: time.struct_time, end: time.struct_time):
 
    return diff
 
-def startTimeLog(config: configparser.ConfigParser):
+def startTimeLog(config: configparser.ConfigParser, desc: str = "", tags: str = ""):
    '''
-   Creates a new entry in the current log file with all the needed data
+   Creates a new entry in the current log file with all the needed data.
+   If there's no current log file, it creates a new one.
    '''
 
-   # NOTE: Get commandlilne (and/or TUI) arguments. i.e.: tags, description, etc...
+   # Read || Create log file
+   storage = config["settings"]["storeDataHere"]
+
+   if (storage == ""):
+      storage = Path.home() / ".logLanceData"
+
+      # Create storage folder if it doesn't already exist
+      if not Path(storage).is_dir():
+         storage.mkdir()
+         print(f"{storage} created!")
+
+   elif not Path(storage).is_dir():
+      print(f"The path you've given in config.ini -> storeDataHere:\n({storage})\nis invalid!\nPlease make sure the folder exists and that the path is correct")
+      exit("err: invalid storage folder path")
+
+   # TODO: Get commandlilne (and/or TUI) arguments. i.e.: tags, description, etc...
    timeFormat = config["settings"]["timeFormat"]
    currentTime = time.gmtime()
 
-   # {{{ Testing params
-   thePast = time.strptime("24-04-15 Mon 11:30:59", timeFormat)
-   # }}}
-
-   
    formattedTime = time.strftime(timeFormat, currentTime)
 
-   print(calcTimeDiff(thePast, currentTime))
+   newTimeLog = TimeEntry(start=formattedTime, end="", desc= desc, tags= tags)
 
-   pass
+   relevantDataFile = open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.data", "a")
+   relevantDataFile.write(newTimeLog)
+   relevantDataFile.close
+
+   ## {{{ Testing params
+   # thePast = time.strptime("24-04-15 Mon 11:30:59", timeFormat)
+   ## }}}
+   # print(calcTimeDiff(thePast, currentTime))
+
 
 def creatDefConfig():
 
