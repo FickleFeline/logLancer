4087fd52165f3df832f0060c499cf0e6b0903cf7
FickleFeline <N/A>
2024-06-20 12:47:32 +0200

    sync bump

--- __main__.py
+++ __main__.py
@@ -22,26 +22,14 @@ def calcTimeDiff(start: time.struct_time, end: time.struct_time):
 
    return diff
 
-def startTimeLog(config: configparser.ConfigParser, desc: str = "", tags: str = ""):
+def startTimeLog(config: configparser.ConfigParser, storage : Path | str = "", desc: str = "", tags: str = ""):
    '''
    Creates a new entry in the current log file with all the needed data.
    If there's no current log file, it creates a new one.
    '''
 
    # Read || Create log file
-   storage = config["settings"]["storeDataHere"]
-
-   if (storage == ""):
-      storage = Path.home() / ".logLanceData"
-
-      # Create storage folder if it doesn't already exist
-      if not Path(storage).is_dir():
-         storage.mkdir()
-         print(f"{storage} created!")
-
-   elif not Path(storage).is_dir():
-      print(f"The path you've given in config.ini -> storeDataHere:\n({storage})\nis invalid!\nPlease make sure the folder exists and that the path is correct")
-      exit("err: invalid storage folder path")
+   fields = config["expansions"]["fields"].split(",")
 
    # TODO: Get commandlilne (and/or TUI) arguments. i.e.: tags, description, etc...
    timeFormat = config["settings"]["timeFormat"]
@@ -49,11 +37,18 @@ def startTimeLog(config: configparser.ConfigParser, desc: str = "", tags: str = 
 
    formattedTime = time.strftime(timeFormat, currentTime)
 
-   newTimeLog = TimeEntry(start=formattedTime, end="", desc= desc, tags= tags)
+   newTimeLog = {"startTime":formattedTime, "endTime":"", "description": desc, "tags": tags}
 
-   relevantDataFile = open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.data", "a")
-   relevantDataFile.write(newTimeLog)
-   relevantDataFile.close
+   if not Path("{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv").exists():
+      with open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv", "w") as logData:
+         writer = csv.DictWriter(logData, fieldnames=fields)
+         writer.writeheader()
+         writer.writerow(newTimeLog)
+         print(f"File written to: {storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv")
+
+   # with open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv", "a") as logData:
+      # reader = csv.DictReader(logData)
+      # logs = [row for row in reader]
 
    ## {{{ Testing params
    # thePast = time.strptime("24-04-15 Mon 11:30:59", timeFormat)
@@ -73,7 +68,22 @@ def main():
    config = configparser.ConfigParser()
    _ =config.read("./config.ini")
 
-   startTimeLog(config)
+   #init storage folder:
+   storage = config["settings"]["storeDataHere"]
+   if (storage == ""):
+      storage = Path.home() / ".logLanceData"
+
+      # Create storage folder if it doesn't already exist
+      if not Path(storage).is_dir():
+         storage.mkdir()
+         print(f"{storage} created!")
+
+   elif not Path(storage).is_dir():
+      print(f"The path you've given in config.ini -> storeDataHere:\n({storage})\nis invalid!\nPlease make sure the folder exists and that the path is correct")
+      exit("err: invalid storage folder path")
+
+
+   startTimeLog(config, storage = storage)
 
    return
 
