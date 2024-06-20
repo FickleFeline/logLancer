18263aa25755163ef2f7fe55583e954fa68982a6
FickleFeline <N/A>
2024-06-20 17:51:36 +0200

    dynamic new entry creation kinda done
    it now creates a new time log file if there's no existing one and
    populates it with a new entry.
    
    +made some .gitignore changes...

--- __main__.py
+++ __main__.py
@@ -22,38 +22,108 @@ def calcTimeDiff(start: time.struct_time, end: time.struct_time):
 
    return diff
 
-def startTimeLog(config: configparser.ConfigParser, storage : Path | str = "", desc: str = "", tags: str = ""):
+def startTimeLog(config: configparser.ConfigParser, pathToCurrentLog: Path | str, args: dict):
    '''
    Creates a new entry in the current log file with all the needed data.
    If there's no current log file, it creates a new one.
    '''
-
-   # Read || Create log file
-   fields = config["expansions"]["fields"].split(",")
-
-   # TODO: Get commandlilne (and/or TUI) arguments. i.e.: tags, description, etc...
-   timeFormat = config["settings"]["timeFormat"]
-   currentTime = time.gmtime()
-
-   formattedTime = time.strftime(timeFormat, currentTime)
-
-   newTimeLog = {"startTime":formattedTime, "endTime":"", "description": desc, "tags": tags}
-
-   if not Path("{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv").exists():
-      with open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv", "w") as logData:
-         writer = csv.DictWriter(logData, fieldnames=fields)
+   
+   # TODO: Check for the least line of the file and
+   # - If it has no end time ask user to confirm new log.
+   # -- If they answer yes: add current time to endTime
+   # -- if no: exith with an explanation.
+   # - Else append the csv file with the new log.
+   if not Path(pathToCurrentLog).exists(): # if it doesn't exist creat and append the new log entry to it
+      with open(pathToCurrentLog, 'a', newline='') as csvfile:
+         writer = csv.DictWriter(csvfile, fieldnames=args.keys())
          writer.writeheader()
-         writer.writerow(newTimeLog)
-         print(f"File written to: {storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv")
+         writer.writerow(args)
+         # TODO: Figure out how to only have one title row when appending instead of adding one with every new appending of the file?
+         ###
+      return
 
-   # with open(f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}.csv", "a") as logData:
-      # reader = csv.DictReader(logData)
-      # logs = [row for row in reader]
+   #else chechk the last 2 lines for an endTime key and value
+   newestEntryRaw = lastNlines(pathToCurrentLog, 2)
+   newestEntryProcessed = {}
+   reader = csv.DictReader(newestEntryRaw)
+   for row in reader:
+      newestEntryProcessed = row
+   if newestEntryProcessed["endTime"] == "":
+      print("No end time found!\nWould you like to end the currently running entry and start a new one with the provided details?")
+      # {{{ Testing params
+      consent = True
+      # }}}
 
-   ## {{{ Testing params
-   # thePast = time.strptime("24-04-15 Mon 11:30:59", timeFormat)
-   ## }}}
-   # print(calcTimeDiff(thePast, currentTime))
+      if consent:
+         # TODO: In the csv file set an endTime to last entry and appent the new one to the end of the file.
+
+         pass #placeholder pass
+      else:
+         print("Understood. Exiting w/o making any changes")
+         return
+
+   else:
+      print(f"End time found: {newestEntryProcessed["endTime"]}")
+
+# Function to read
+# last N lines of the file
+def lastNlines(fname, N):
+   #Definitely not copy pasted from https://www.geeksforgeeks.org/python-reading-last-n-lines-of-a-file/ ...
+     
+    # assert statement check
+    # a condition
+    assert N >= 0
+     
+    # declaring variable
+    # to implement 
+    # exponential search
+    pos = N + 1
+     
+    # list to store
+    # last N lines
+    lines = []
+     
+    # opening file using with() method
+    # so that file get closed
+    # after completing work
+    with open(fname) as f:
+         
+        # loop which runs
+        # until size of list
+        # becomes equal to N
+        while len(lines) <= N:
+             
+            # try block
+            try:
+                # moving cursor from
+                # left side to
+                # pos line from end
+                f.seek(-pos, 2)
+         
+            # exception block 
+            # to handle any run 
+            # time error
+            except IOError:
+                f.seek(0)
+                break
+             
+            # finally block 
+            # to add lines 
+            # to list after
+            # each iteration
+            finally:
+                lines = list(f)
+             
+            # increasing value
+            # of variable
+            # exponentially
+            pos *= 2
+             
+    # returning the
+    # whole list
+    # which stores last
+    # N lines
+    return lines[-N:]
 
 
 def creatDefConfig():
@@ -83,7 +153,32 @@ def main():
       exit("err: invalid storage folder path")
 
 
-   startTimeLog(config, storage = storage)
+   timeFormatInStorage = config["settings"]["timeFormatInStorage"]
+   currentTime = time.gmtime()
+
+   formattedTime = time.strftime(timeFormatInStorage, currentTime)
+
+   pathToCurrentLog = f"{storage}/{currentTime.tm_year}-{currentTime.tm_mon}-{currentTime.tm_mday}.csv"
+
+   # TODO: Get commandlilne (and/or TUI) arguments. i.e.: tags, description, etc...
+   #{{{ PLACHOLDER VARIABLES TO BE EVENTUALLY CHANGED INTO ARGS
+   description = "this is a task description"
+   tags = ["FT", "00"]
+   #}}}
+
+   logArgs = {}
+   logArgs["starTime"] = formattedTime
+   logArgs["endTime"] = ""
+   logArgs["desc"] = description
+   logArgs["tags"] = tags
+   
+
+   startTimeLog(config= config, pathToCurrentLog= pathToCurrentLog, args= logArgs)
+
+   ## {{{ Testing params
+   # thePast = time.strptime("24-04-15 Mon 11:30:59", timeFormatInStorage)
+   ## }}}
+   # print(calcTimeDiff(thePast, currentTime))
 
    return
 
